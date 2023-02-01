9d8033b46a517136111da12598fca391f6595d1c
pkarisa <faith.pendo@student.moringaschool.com>
2023-02-01 21:05:41 +0300

    update

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
+const mongoose = require("mongoose");
+
+let DB_NAME = process.env.DB_NAME;
+let DB_USER = process.env.DB_USER;
+let DB_PASS = process.env.DB_PASS;
+let DB_URL = process.env.DB_URL
+let db_url = "mongodb+srv://" + DB_USER + ":" + DB_PASS + "@galleryapp.xziuwg7.mongodb.net/" + DB_NAME;
+console.log(db_url);
+let option = {
+   maxPoolSize: 10,
+   family: 4
+};
+mongoose.connect(db_url, option);
+let db = mongoose.connection
+db.once("open", () => {
+   console.log("Successful connection to the database");
+});
 app.get('/',(req, res)=> {
   res.sendFile(__dirname + '/index.html')
  })
