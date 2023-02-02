4f2e076b6c771ec9afed1ea0c049eaa2f119f43f
pkarisa <faith.pendo@student.moringaschool.com>
2023-02-02 22:29:46 +0300

    update

--- server.js
+++ server.js
@@ -1,36 +1,59 @@
-const express = require('express')
-const bodyParser= require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-const app = express()
+const app = express();
 
 // Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }))
-app.listen(3000, function() {
-  console.log('listening on 3000')
+
+app.listen(3000, function () {
+    console.log("listening on 3000")
 })
-const mongoose = require("mongoose");
 
 let DB_NAME = process.env.DB_NAME;
 let DB_USER = process.env.DB_USER;
 let DB_PASS = process.env.DB_PASS;
 let DB_URL = process.env.DB_URL
-let db_url = "mongodb+srv://" + DB_USER + ":" + DB_PASS + "@galleryapp.xziuwg7.mongodb.net/" + DB_NAME;
+let db_url = "mongodb+srv://" + DB_USER + ":" + DB_PASS + "@starwarsapp.nbk2k5v.mongodb.net/" + DB_NAME;
 console.log(db_url);
 let option = {
-   maxPoolSize: 10,
-   family: 4
+    maxPoolSize: 10,
+    family: 4
 };
 mongoose.connect(db_url, option);
 let db = mongoose.connection
 db.once("open", () => {
-   console.log("Successful connection to the database");
+    console.log("Successful connection to the database");
 });
-app.get('/',(req, res)=> {
-  res.sendFile(__dirname + '/index.html')
- })
 
-// All your handlers here...
+db.on("error", () => {
+    console.log("Connection to DB failed");
+});
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    secondName: { type: String, required: true },
+    emailAddress: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    timmestamp: { type: Date, default: Date.now, immutable: true }
+
+});
+
+let User = mongoose.model('User', userSchema);
+let user;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
-  })
-   
+    user = {
+        firstName: req.body.firstName,
+        secondName: req.body.secondName,
+        emailAddress: req.body.email,
+        password: req.body.pass,
+    }
+
+    let newUser = new User(user);
+    newUser.save();
+    res.redirect('/');
+})
